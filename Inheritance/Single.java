5e322b572486645e4c1a51687019d16bfb820a72
Poojith Ganesh Marem <159864736+PoojithGanesh@users.noreply.github.com>
2026-01-22 09:54:35 +0530

    Add Vehicle and R192311290 classes with display methods

---
+++ Inheritance/Single.java
@@ -0,0 +1,18 @@
+class Vehicle{
+    void vehicledisplay(){
+        System.out.println("vehicle class");
+    }
+}
+
+class R192311290 extends Vehicle{
+        void twowheelerdisplay(){
+            System.out.println("2 wheeler");
+        }
+        public static void main(String[] args){
+        R192311290 t = new R192311290();
+        t.vehicledisplay();
+        t.twowheelerdisplay();
+        }
+    }
+    
+   

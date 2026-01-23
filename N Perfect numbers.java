8ee00b6e959382f124f756c865f7a6d958c033dc
Poojith Ganesh Marem <159864736+PoojithGanesh@users.noreply.github.com>
2026-01-23 09:26:18 +0530

    Update and rename N perfect numbers.java to N Perfect numbers.java

--- N Perfect numbers.java
+++ N Perfect numbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class PerfectNumbers {
+public class PerfectNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n value: ");

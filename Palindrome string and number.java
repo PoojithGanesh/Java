609f043ed79f533cf05b7f12d959ec61ecf38acc
Poojith Ganesh Marem <159864736+PoojithGanesh@users.noreply.github.com>
2026-01-23 18:09:38 +0530

    Refactor palindrome checker for strings and numbers

--- Palindrome string and number.java
+++ Palindrome string and number.java
@@ -1,34 +1,42 @@
 import java.util.Scanner;
-public class PalindromeCheck {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+class R192311290{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Choose option:");
+    System.out.println("1. String Palindrome");
+    System.out.println("2. Number Palindrome");
+    int choice = sc.nextInt();
+    sc.nextLine();
+    switch(choice){
+      case 1:
         System.out.print("Enter a string: ");
-        String s = sc.nextLine();  
+        String s = sc.nextLine();
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--){
+          rev += s.charAt(i);
+        }
+        if (s.equals(rev))
+          System.out.println("String is Palindrome");
+        else
+          System.out.println("String is Not Palindrome");
+        break;
+      
+      case 2:
         System.out.print("Enter a number: ");
-        int num = sc.nextInt();    
-
-        String revStr = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            revStr = revStr + s.charAt(i);
+        int num = sc.nextInt();
+        int temp = num, r, revNum = 0;
+        while (num > 0){
+          r = num % 10;
+          revNum = revNum * 10 + r;
+          num = num / 10;
         }
-
-        if (s.equals(revStr))
-            System.out.println("Palindrome String");
-        else
-            System.out.println("Not Palindrome String");
-
-        int temp = num;
-        int revNum = 0;
-
-        while (num > 0) {
-            int digit = num % 10;
-            revNum = revNum * 10 + digit;
-            num = num / 10;
-        }
-
         if (temp == revNum)
-            System.out.println("Palindrome Number");
+          System.out.println("Number is Palindrome");
         else
-            System.out.println("Not Palindrome Number");
+          System.out.println("Number is Not Palindrome");
+        break;
+      default:
+        System.out.println("Invalid option");
     }
+  }
 }

b06f9482bf1b80154ad925bb8bf3942df08f167a
Poojith Ganesh Marem <159864736+PoojithGanesh@users.noreply.github.com>
2026-01-23 18:26:37 +0530

    Refactor palindrome checker with switch case

--- Palindrome string and number.java
+++ Palindrome string and number.java
@@ -1,42 +1,28 @@
 import java.util.Scanner;
-class R192311290{
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Choose option:");
-    System.out.println("1. String Palindrome");
-    System.out.println("2. Number Palindrome");
-    int choice = sc.nextInt();
-    sc.nextLine();
-    switch(choice){
-      case 1:
-        System.out.print("Enter a string: ");
-        String s = sc.nextLine();
-        String rev = "";
-        for (int i = s.length() - 1; i >= 0; i--){
-          rev += s.charAt(i);
+public class PalindromeSwitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("choose option");
+        System.out.println("1.String");
+        System.out.println("2.Number");
+        int ch = sc.nextInt();
+
+        switch (ch) {
+            case 1:
+                String s = sc.next();
+                String r = "";
+                for (int i = s.length()-1; i >= 0; i--)
+                    r += s.charAt(i);
+                System.out.println(s.equals(r) ? "string Palindrome" : "Not string Palindrome");
+                break;
+
+            case 2:
+                int n = sc.nextInt(), rev = 0, t = n;
+                while (n > 0) {
+                    rev = rev * 10 + n % 10;
+                    n /= 10;
+                }
+                System.out.println(t == rev ? "number Palindrome" : "Not number Palindrome");
         }
-        if (s.equals(rev))
-          System.out.println("String is Palindrome");
-        else
-          System.out.println("String is Not Palindrome");
-        break;
-      
-      case 2:
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-        int temp = num, r, revNum = 0;
-        while (num > 0){
-          r = num % 10;
-          revNum = revNum * 10 + r;
-          num = num / 10;
-        }
-        if (temp == revNum)
-          System.out.println("Number is Palindrome");
-        else
-          System.out.println("Number is Not Palindrome");
-        break;
-      default:
-        System.out.println("Invalid option");
     }
-  }
 }

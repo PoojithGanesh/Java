9b6f9c625a768596126448a449556fd59b3b75c2
Poojith Ganesh Marem <159864736+PoojithGanesh@users.noreply.github.com>
2026-02-01 12:11:17 +0530

    Add Binary to Decimal converter in Java

---
+++ Binary to Decimal.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class R192311290{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the binary:");
+        String n = sc.next();
+        int decimal = Integer.parseInt(n, 2);
+        System.out.println("Decimal : " + decimal);
+    }
+}
